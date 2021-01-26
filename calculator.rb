53567d215ef1b5468e478be39380c44fbafc0937
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-26 21:43:54 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,11 @@
 # Add your variables here
+first_number= 2
+second_number=4
+sum=6
+difference= -2
+product = 8
+quotient = 0
+first_number + second_number 
+first_number - second_number
+first_number * second_number
+first_number/second_number
